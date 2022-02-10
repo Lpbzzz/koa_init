991b6380f6d13aed524234dc104a6793ecd3aff7
lpb <462570646@qq.com>
2022-02-10 17:16:20 +0800

    cros

--- src/app.js
+++ src/app.js
@@ -3,7 +3,12 @@ var bodyParser = require("koa-bodyparser");
 
 var app = new Koa();
 app.use(bodyParser());
-
+app.use(async (ctx, next)=> {
+	ctx.set('Access-Control-Allow-Origin', '*');
+	ctx.set('Access-Control-Allow-Headers', 'Content-Type');
+	ctx.set('Access-Control-Allow-Methods', 'POST');
+	await next();
+  });
 app.use(async (ctx) => {
 	ctx.body = ctx.request.body;
 });
